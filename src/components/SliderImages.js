a4efd4b3bed15f88ec30bcf0f9f1b2b70329235e
Shirish Kulkarni <shirishkpl109@gmail.com>
2022-01-16 16:44:12 +0530

    first commit

--- src/components/SliderImages.js
+++ src/components/SliderImages.js
@@ -10,10 +10,10 @@ function SliderImages() {
                 src="https://pbs.twimg.com/media/DXLmtgXV4AAJENl.jpg:large"
                 alt="First slide"
                 />
-                <Carousel.Caption>
+                {/* <Carousel.Caption>
                 <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+                </Carousel.Caption> */}
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -21,10 +21,10 @@ function SliderImages() {
                 src="https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-my-singapore-connect-0-2020-11-21-t-20-55-54.jpg"
                 alt="Second slide"
                 />
-                <Carousel.Caption>
+                {/* <Carousel.Caption>
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
+                </Carousel.Caption> */}
             </Carousel.Item>
             <Carousel.Item>
                 <img
@@ -32,10 +32,10 @@ function SliderImages() {
                 src="https://m.media-amazon.com/images/I/415b3x%2B%2BjbL.jpg"
                 alt="Third slide"
                 />
-                <Carousel.Caption>
+                {/* <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
+                </Carousel.Caption> */}
             </Carousel.Item>
         </Carousel>
     </div>
