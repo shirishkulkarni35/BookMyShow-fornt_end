f63172a15e28060385847d210822eb9fcd51271f
Shirish Kulkarni <shirishkpl109@gmail.com>
2022-01-16 16:35:44 +0530

    first commig

--- src/components/Premieres.js
+++ src/components/Premieres.js
@@ -12,31 +12,27 @@ function Premieres() {
                     <Card>
                         <Card.Img variant="top" src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/large/coma-et00303851-25-01-2021-03-39-41.jpg" />
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>Coma</Card.Title>
                         <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.
+                            Language: English
                         </Card.Text>
                         </Card.Body>
                     </Card>
                     <Card>
                         <Card.Img variant="top" src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00056556-zftjbeevvq-portrait.jpg" />
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>Fast and Furious</Card.Title>
                         <Card.Text>
-                            This card has supporting text below as a natural lead-in to additional
-                            content.{' '}
+                            Language: English
                         </Card.Text>
                         </Card.Body>
                     </Card>
                     <Card>
                         <Card.Img variant="top" src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/large/ravana-lanka-et00312121-22-06-2021-01-44-29.jpg" />
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>Ravana Lanka</Card.Title>
                         <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to
-                            show that equal height action.
+                            Language: Telugu
                         </Card.Text>
                         </Card.Body>
                     </Card>
@@ -47,31 +43,27 @@ function Premieres() {
                     <Card>
                         <Card.Img variant="top" src="https://in.bmscdn.com/discovery-catalog/events/et00316141-wfaajwbzab-portrait.jpg" />
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>The Sister</Card.Title>
                         <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.
+                            Language: English
                         </Card.Text>
                         </Card.Body>
                     </Card>
                     <Card>
                         <Card.Img variant="top" src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/large/trip-et00126236-16-12-2020-02-06-35.jpg" />
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>Trip</Card.Title>
                         <Card.Text>
-                            This card has supporting text below as a natural lead-in to additional
-                            content.{' '}
+                            Language: Tamil
                         </Card.Text>
                         </Card.Body>
                     </Card>
                     <Card>
                         <Card.Img variant="top" src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/large/dhaakad-et00106640-18-01-2021-01-32-10.jpg" />
                         <Card.Body>
-                        <Card.Title>Card title</Card.Title>
+                        <Card.Title>Dhaakad</Card.Title>
                         <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to
-                            show that equal height action.
+                            Language: Hindi
                         </Card.Text>
                         </Card.Body>
                     </Card>
